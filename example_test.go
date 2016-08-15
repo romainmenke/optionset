bc91d2aaf715122a8505946c5792e2597cdf45e4
romainmenke <romainmenke@gmail.com>
2016-08-15 15:49:52 +0200

    clearer example

--- example_test.go
+++ example_test.go
@@ -9,7 +9,7 @@ import (
 type AnimalTrait uint32
 
 const (
-	Fluffy AnimalTrait = iota + 1
+	Fluffy AnimalTrait = 1 << iota
 	EatsMeat
 	EatsPlants
 	Flying
@@ -24,6 +24,27 @@ func (t AnimalTrait) RawValue() uint32 {
 
 func (t AnimalTrait) FromRaw(raw uint32) optionset.Option {
 	return AnimalTrait(raw)
+}
+
+func (t AnimalTrait) String() string {
+	switch t.RawValue() {
+	case 1:
+		return "Fluffy"
+	case 2:
+		return "Eats Meat"
+	case 4:
+		return "Eats Plants"
+	case 8:
+		return "Flying"
+	case 16:
+		return "Swimming"
+	case 32:
+		return "Friendly"
+	case 64:
+		return "Wild"
+	default:
+		return "Unknown"
+	}
 }
 
 func Example() {
